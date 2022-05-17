fa3578589edbc46d14831af2e911e7475ec92329
oceansalle <oceanesalleiae@gmail.com>
2022-05-17 16:49:31 +0200

    header component

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './header.css'
+import Button from './Button'
+import profile from '../../assets/profilepic.jpg'
+import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Hajiba Bintu</h1>
+        <h5 className="text-light">Fullstack Developer</h5> 
+        <Button />
+        <HeaderSocials />
+
+          <div className="me">
+            <img src={profile} alt="profile" />
+          </div>
+
+          <a href="#contact" clasName="scroll__down">Scroll Down</a>
+
+          
+      </div>
+    </header>
   )
 }
 
