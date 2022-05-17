c1f8d4cacbcb2b1342b22d298f8c39525c4e35e1
oceansalle <oceanesalleiae@gmail.com>
2022-05-17 15:57:57 +0200

    creating component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Blog from './components/blog/Blog'
+import Footer from './components/footer/Footer'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Navbar />
+      <About/>
+      <Contact/>
+      <Portfolio/>
+      <Services/>
+      <Blog/>
+      <Footer/>
+
+
+
+    </div>
   )
 }
 
